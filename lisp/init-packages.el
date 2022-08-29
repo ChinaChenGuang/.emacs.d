f45068fbb46e9584299bc2319924aec873cc0ed2
gchen <1443645743@qq.com>
2022-08-29 16:50:13 +0800

    update

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -7,6 +7,16 @@
 ;;donwload auto-complete and dependence
 (use-package auto-complete)
 (use-package pos-tip)
-(use-package neotree)
+;;(use-package neotree
+;;  :ensure t
+;;  :bind-keymap ([F8] . neotree-toggle))
 (use-package fuzzy)
+(use-package avy
+  :ensure t
+  :bind (("C-:" . avy-goto-char))
+  )
+(use-package ivy
+  :ensure t
+  :diminish ivy-mode
+  :hook(after-init . ivy-mode))
 (provide 'init-packages)
